07c9f4a436d3078b212ac841b913372f12595970
yoyo1025 <hsjejxys@gmail.com>
2024-03-25 00:34:57 +0900

    react初級編 station3クリア

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ export const App = () => {
   return (
     <div>
       <header>Dogアプリ</header>
+      <p>イヌの画像を表示するサイト</p>
+      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" alt="dog" />
     </div>
   )
 }
