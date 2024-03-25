95f3b006f66520a64749d09028d24f12d8e80f65
yoyo1025 <hsjejxys@gmail.com>
2024-03-25 19:38:43 +0900

    react初級編 station4, 5, 6 クリア

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 // DO NOT DELETE
 
 import './App.css'
@@ -6,11 +7,27 @@ import './App.css'
  * @type {() => JSX.Element}
  */
 export const App = () => {
+
+  const [ dogUrl, setDogUrl ] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
+
+  const changePicture = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setDogUrl(result.message);
+        }
+      )
+  }
+
   return (
     <div>
       <header>Dogアプリ</header>
       <p>イヌの画像を表示するサイト</p>
-      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" alt="dog" />
+      <img src={dogUrl} alt="dog" />
+      <div>
+        <button onClick={changePicture}>更新</button>
+      </div>
     </div>
   )
 }
