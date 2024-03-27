8ef349bf5eb57d214c1d5e5458faef054d5e6dce
yoyo1025 <hsjejxys@gmail.com>
2024-03-27 13:40:27 +0900

    react初級編 station9クリア

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Description from './Description';
+import DogListContainer from './DogListContainer';
 
 export const App = () => {
   return (
     <div>
       <Header />
       <Description />
+      <DogListContainer />
     </div>
   )
 }
--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,6 +1,22 @@
+import { useState, useEffect } from "react";
 // @ts-check
 
 export const DogListContainer = () => {
+  const [breeds, setBreeds] = useState();
+
+  useEffect(
+    () => {
+      fetch("https://dog.ceo/api/breeds/list/all")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setBreeds(result.message);
+          }
+        )
+    }
+    ,
+    []
+  )
   return <></>
 }
 
